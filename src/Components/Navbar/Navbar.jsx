78f559bf1d6aa982daa7ca4661e99c4eaa32d9b4
ManikantaGalidevara <manikantag1519@gmail.com>
2025-05-11 01:35:57 +0530

    changes4

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 
-class Navbar extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            islogin : false
-        }
-        
-    }
-    handleLogin = ()=>{
-            this.setState({islogin : !this.state.islogin})
-        }
-    render(){
-        return(
-            <div className="navbar">
-                <button onClick={this.props.colorprop}>multi colors</button>
-                <button onClick={this.props.otpprop}>opt generator</button>
-                <button onClick={this.props.quotesprop}>random quotes</button>
-                <button onClick={this.props.imagesprops}>random images</button>
-                <button onClick={this.props.themeprops}>theme</button>
-                <button onClick={this.props.loginprop}>Login</button>
-                <button onClick={this.props.signupprop}>sign up</button>
-                
-            </div>
-        )
-    }
+class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      islogin: false,
+    };
+  }
+  handleLogin = () => {
+    this.setState({ islogin: !this.state.islogin });
+  };
+  render() {
+    return (
+      <div >
+        <div className="navbar">
+          <button onClick={this.props.colorprop}>multi colors</button>
+          <button onClick={this.props.otpprop}>opt generator</button>
+          <button onClick={this.props.quotesprop}>random quotes</button>
+        </div>
+        <div className="navbar">
+          <button onClick={this.props.imagesprops}>random images</button>
+          <button onClick={this.props.themeprops}>theme</button>
+          <button onClick={this.props.loginprop}>Login</button>
+          <button onClick={this.props.signupprop}>sign up</button>
+        </div>
+      </div>
+    );
+  }
 }
 export default Navbar;
